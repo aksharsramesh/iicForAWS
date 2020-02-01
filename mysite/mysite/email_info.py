5370139a4cb4c7c5e236dd8dbbf56870bcb67b83
aksharsramesh <41045722+aksharsramesh@users.noreply.github.com>
2020-02-01 13:02:46 +0530

    Update email_info.py

--- mysite/mysite/email_info.py
+++ mysite/mysite/email_info.py
@@ -1,5 +1,10 @@
+#EMAIL_USETLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'djangodevstaff@gmail.com'
+#EMAIL_HOST_PASSWORD = 'staff@2000'
+#EMAIL_PORT = 587
 EMAIL_USETLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'djangodevstaff@gmail.com'
-EMAIL_HOST_PASSWORD = 'staff@2000'
+EMAIL_HOST_USER = 'iicjssstu2020ideathon@gmail.com'
+EMAIL_HOST_PASSWORD = 'ideathon2020'
 EMAIL_PORT = 587
