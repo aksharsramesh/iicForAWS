24e3d887817865fda3293e52c5805e43113e0ef5
Ubuntu <ubuntu@ip-172-31-36-138.ap-south-1.compute.internal>
2020-02-03 16:55:31 +0000

    everything i guess

--- mysite/mysite/email_info.py
+++ mysite/mysite/email_info.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-EMAIL_USE_TLS = True
-=======
-#EMAIL_USETLS = True
+#EMAIL_USE_TLS = True
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_HOST_USER = 'djangodevstaff@gmail.com'
 #EMAIL_HOST_PASSWORD = 'staff@2000'
 #EMAIL_PORT = 587
-EMAIL_USETLS = True
->>>>>>> 5370139a4cb4c7c5e236dd8dbbf56870bcb67b83
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'iicjssstu2020ideathon@gmail.com'
 EMAIL_HOST_PASSWORD = 'ideathon2020'
