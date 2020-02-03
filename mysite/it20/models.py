93294b533bbf1028a2521880a2319479dcc213d0
aksharsramesh <41045722+aksharsramesh@users.noreply.github.com>
2020-02-03 19:41:06 +0530

    Update models.py

--- mysite/it20/models.py
+++ mysite/it20/models.py
@@ -6,7 +6,7 @@ from datetime import date
 class newIdea(models.Model):
     title = models.CharField(max_length = 100)
     author = models.CharField(max_length = 100)
-    UPI = models.CharField(max_length = 100, default = "Your Transaction ID with anju.marina.lobo@oksbi HERE")
+    UPI = models.CharField(max_length = 100)
     document = models.FileField(upload_to='documents/', blank=True)
     date = models.DateTimeField(auto_now_add = True)
     contact = models.CharField(max_length = 13)
