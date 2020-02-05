51f4d9937041a645fbc8433865572d8a15fed25a
aksharsramesh <41045722+aksharsramesh@users.noreply.github.com>
2020-02-05 23:54:27 +0530

    Update models.py

--- mysite/it20/models.py
+++ mysite/it20/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 from datetime import date
 
 class newIdea(models.Model):
-    title = models.CharField(max_length = 100)
+    Title_Of_Idea = models.CharField(max_length = 100)
     author = models.CharField(max_length = 100)
     UPI_transaction_ID = models.CharField(max_length = 100)
     document = models.FileField(upload_to='documents/', blank=True)
     date = models.DateTimeField(auto_now_add = True)
-    contact = models.CharField(max_length = 13)
+    Contact_Number = models.CharField(max_length = 13)
 
     def __str__(self):
         return self.title
