bbe63c61efd53b6498318b6d29a484e7344a6e3c
aksharsramesh <41045722+aksharsramesh@users.noreply.github.com>
2020-02-17 21:43:46 +0530

    Update forms.py

--- mysite/it20/forms.py
+++ mysite/it20/forms.py
@@ -14,4 +14,4 @@ class SignupForm(UserCreationForm):
 class NEWIdeaSubmissionForm(ModelForm):
     class Meta:
         model = models.newIdea
-        fields = ['Title_Of_Idea', 'Contact_Number', 'UPI_transaction_ID', 'document']
+        fields = ['Title_Of_Idea', 'Contact_Number', 'UPI_transaction_ID', 'Your_UPI_ID', 'document']
