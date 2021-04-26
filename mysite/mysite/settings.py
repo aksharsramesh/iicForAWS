82f37aba34a80cc3aab195d76af9489ddca2bdee
Ubuntu <ubuntu@ip-172-31-47-135.ap-south-1.compute.internal>
2021-04-26 17:33:53 +0000

    26 April 2021. 9PM

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+@+%hno9hcy7y2w9d_lr%t0%%&rs!x(4gws97tgd8t9jb9lug^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['13.127.23.59','localhost','127.0.0.1']
 
 
 # Application definition
